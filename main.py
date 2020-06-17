d81b515dc69c4049e69e1d00a01167a7477060a5
Max Hacicheant <max.hacicheant@gmail.com>
2020-06-17 22:00:48 +0300

    Update formating

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ o = input("Enter an operation\t")
 if o == "+":
     print(a + b)
 
-elif o == "-":
+elif o == "-":   # decreasing b by a
     print(a - b)
 
 elif o == "/":
-    print(a / b)
+    if a or b == 0:
+        print("Error: Division by 0")
+    else:
+        print(a / b)
 
 elif o == "*":
     print(a * b)
 
 elif o == "mod":
-    if b == 0:
-        print("Error: Division by 0")
-    else:
-        print(a % b)
+    print(a % b)
 
 elif o == "pow":
     print(a ** b)
 
 elif o == "div":
-    if b == 0:
+    if a or b == 0:
         print("Error: Division by 0")
     else:
         print(a // b)
